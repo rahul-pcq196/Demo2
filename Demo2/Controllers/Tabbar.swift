c3137277b01f0a017216c9c5dffc98cfb9c17dd5
rahul-pcq196 <78337365+rahul-pcq196@users.noreply.github.com>
2021-02-02 11:29:36 +0530

    Add files via upload

---
+++ Demo2/Controllers/Tabbar.swift
@@ -0,0 +1,18 @@
+//
+//  Tabbar.swift
+//  Demo2
+//
+//  Created by PCQ196 on 01/02/21.
+//
+
+import UIKit
+
+class Tabbar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}
